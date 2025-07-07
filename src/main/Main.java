1f515c50b1fae23443d1bca161c29568b7274dd7
jerome-cossu <jerome.cossu@laplateforme.io>
2025-07-07 12:27:31 +0200

    🐛

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,16 @@
 package src.main;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/src/view/login_page.fxml"));
+        Parent root = FXMLLoader.load(new File("src/view/login_page.fxml").toURI().toURL());
         primaryStage.setTitle("Connexion - La Plateforme Tracker");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
